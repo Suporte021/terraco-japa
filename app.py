469e1de77ca92006603b993ee1b18eb6a0476486
Suporte021 <155651017+Suporte021@users.noreply.github.com>
2026-03-06 21:10:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from Flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
-@spp.route("/")
+@app.route("/")
 def home():
-  return "erraco japa rodando"
+    return "Terraço Japa funcionando 🍣"
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
